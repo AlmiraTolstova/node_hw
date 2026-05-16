6fc3caa3af70bcf1bba8bd526d20f5bc1f59fc56
Almira <almiratostova@gmail.com>
2026-05-16 18:42:46 +0200

    task8

---
+++ hw_8/migrations/20260516163245-create-books-table.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("Books", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    size: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("Books");
+}
