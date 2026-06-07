a483ede1ba34265a77c10c478d930aca05a084a3
Almira <almiratostova@gmail.com>
2026-06-07 17:24:39 +0200

    task 2.3 have been done

--- hw_14/app.js
+++ hw_14/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import Product from "./models/Product.js";
+import Category from "./models/Category.js";
 
 // import cartRoutes from "./routes/cartRoutes.js";
 
@@ -16,7 +18,46 @@ app.use(express.json());
 
 connectDB();
 
-// app.use("/api/cart", cartRoutes);
+// CREATE POST /products
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to create product",
+      error: error.message,
+    });
+  }
+});
+
+// CREATE CATEGORY
+app.post("/categories", async (req, res, next) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /products Использование populate
+app.get("/products", async (req, res) => {
+  try {
+    let products = await Product.find().populate("category");
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error getting products",
+      error: error.message,
+    });
+  }
+});
 
 startServer();
 
---
+++ hw_14/models/Product.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
